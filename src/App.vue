a1b0a7cfdcb89d17f6280dcdf6b5aaee4002828d
梶田英完 <hidemasa_kajita@respawn.jp>
2021-06-22 22:08:21 +0900

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <v-app :style="{ background: $vuetify.theme.themes.light.background }">
+  <v-app>
     <v-main>
+      <Header />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
   name: "App",
+  components: {
+    Header,
+  },
 
   data: () => ({
     //
